255641d6c840b3d35ba04711296ad1ef9a9dc356
Jefferson Simão Gonçalves <gerson.simao.92@gmail.com>
2018-06-08 14:55:56 -0300

    Update

---
+++ src/Plugin.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jefferson Simão Gonçalves
+ * Email: gerson.simao.92@gmail.com
+ * Date: 08/06/2018
+ * Time: 14:51
+ */
+
+namespace RestApi;
+
+use Cake\Core\BasePlugin;
+use Cake\Core\Configure;
+use Cake\Core\PluginApplicationInterface;
+use RestApi\Middleware\RestApiMiddleware;
+
+/**
+ * Class Plugin
+ *
+ * @author Jefferson Simão Gonçalves <gerson.simao.92@gmail.com>
+ *
+ * @package RestApi
+ */
+class Plugin extends BasePlugin
+{
+    /**
+     * @param \Cake\Http\MiddlewareQueue $middleware
+     *
+     * @return \Cake\Http\MiddlewareQueue
+     */
+    public function middleware($middleware)
+    {
+        $middleware->add(new RestApiMiddleware());
+
+        return $middleware;
+    }
+
+    public function bootstrap(PluginApplicationInterface $app)
+    {
+        parent::bootstrap($app);
+        try {
+            Configure::load('RestApi.api', 'default', false);
+            Configure::load('api', 'default', true);
+        } catch (\Exception $e) {
+            // nothing
+        }
+    }
+}
